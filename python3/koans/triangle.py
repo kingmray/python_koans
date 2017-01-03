aaa87698325a9f0b4c1f597f7d53ffa2cac725ec
raymond schmidt <raymond.schmidt@googlemail.com>
2017-01-03 12:49:07 +0100

    fixed triangle

--- python3/koans/triangle.py
+++ python3/koans/triangle.py
@@ -16,12 +16,23 @@
 # and
 #   about_triangle_project_2.py
 #
+def sum_of_two_sides_greater_than_third(a, b, c):
+	print('a: {0}, b: {1}, c: {2}'.format(a, b, c))
+	if (a + b < c) or (b + c < a) or (a + c < b):
+		return False
+	return True
+
+def greater_than_zero(a, b, c):
+	return all(i > 0 for i in [a, b, c])
+
 def triangle(a, b, c):
-    if (a == b == c):
-    	return 'equilateral'
-    if (a == b or a == c or b == c ):
-    	return 'isosceles'
-    return 'scalene'
+	if ( a + b + c == 0 or not greater_than_zero(a, b, c) or not sum_of_two_sides_greater_than_third(a, b, c)):
+		raise TriangleError('A very specific bad thing happened')
+	if (a == b == c):
+		return 'equilateral'
+	if (a == b or a == c or b == c ):
+		return 'isosceles'
+	return 'scalene'
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError(Exception):
